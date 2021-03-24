8123ec88fdf934386141c8c1f32f063efe2fa422
Aniket Tiwari <aniket.76907@gmail.com>
2021-03-24 21:39:46 +0530

    added basic layout

---
+++ lib/reader.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// i
+class Reader extends StatefulWidget {
+  @override
+  _ReaderState createState() => _ReaderState();
+}
+
+class _ReaderState extends State<Reader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
